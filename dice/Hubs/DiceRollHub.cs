f3390adc4be25d0dbc35f3ce6ae03e119d15dba1
Oli Newsham <scarylooking@users.noreply.github.com>
2021-06-15 17:28:17 +0100

    Sessions in hubs

--- dice/Hubs/DiceRollHub.cs
+++ dice/Hubs/DiceRollHub.cs
@@ -10,6 +10,16 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Dice.Hubs
 {
+    public class GroupJoinMessage
+    {
+        public string Name { get; set; }
+    }
+
+    public class GroupLeaveMessage
+    {
+        public string Name { get; set; }
+    }
+
     public class DiceRollHub : Hub
     {
         private readonly IDiceRollService _diceRollService;
@@ -17,6 +27,30 @@ namespace Dice.Hubs
         public DiceRollHub(IDiceRollService diceRollService)
         {
             _diceRollService = diceRollService;
+        }
+
+        public async Task JoinSession(string name, string sessionId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
+
+            var response = new GroupJoinMessage
+            {
+                Name = name
+            };
+
+            await Clients.Group(sessionId).SendAsync("diceJoin", JsonSerializer.Serialize(response));
+        }
+
+        public async Task LeaveSession(string name, string sessionId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, sessionId);
+
+            var response = new GroupLeaveMessage
+            {
+                Name = name
+            };
+
+            await Clients.Group(sessionId).SendAsync("diceJoin", JsonSerializer.Serialize(response));
         }
 
         public async Task Roll(string name, int dieCount, int faceCount)
@@ -31,7 +65,7 @@ namespace Dice.Hubs
                 Faces = faceCount
             };
 
-            await Clients.All.SendAsync("newRoll", JsonSerializer.Serialize(response));
+            await Clients.All.SendAsync("diceRoll", JsonSerializer.Serialize(response));
         }
     }
 }
